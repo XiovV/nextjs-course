90efa8cd78d0c9c576fb53b6cc068f7f77bb447d
XiovV <xiovv@tutanota.com>
2023-10-06 22:13:56 +0200

    feat: use modal and backdrops components to show the modal when the Delete button is pressed

--- todo-app/src/components/Todo.js
+++ todo-app/src/components/Todo.js
@@ -1,10 +1,28 @@
+import { useState } from "react";
+import Modal from "./Modal";
+import Backdrop from "./Backdrop";
+
 function Todo(props) {
+  const [showModal, setShowModal] = useState(false);
+
+  function deleteHandler() {
+    setShowModal(true);
+  }
+
+  function hideModalHandler() {
+    setShowModal(false);
+  }
+
   return (
     <div className="card">
       <h2>{props.title}</h2>
       <div className="actions">
-        <button className="btn">Delete</button>
+        <button className="btn" onClick={deleteHandler}>
+          Delete
+        </button>
       </div>
+      {showModal && <Modal onCancel={hideModalHandler} />}
+      {showModal && <Backdrop onClick={hideModalHandler} />}
     </div>
   );
 }
