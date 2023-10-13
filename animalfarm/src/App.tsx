b50deba3d5108bc9ba21d81f6c72aef195dabb87
XiovV <xiovv@tutanota.com>
2023-10-13 10:34:00 +0200

    feat(client): add functionality for searching animals

--- animalfarm/src/App.tsx
+++ animalfarm/src/App.tsx
@@ -1,10 +1,49 @@
-import './App.css'
+import { useState } from "react";
+import Animal from "./components/Animal";
 
-function App() {
-
-  return (
-    <div></div>
-  )
+interface Animal {
+  id: number;
+  type: string;
+  name: string;
+  age: number;
 }
 
-export default App
+function App() {
+  const [animals, setAnimals] = useState<Animal[]>([]);
+
+  const searchHandler = async (q: string) => {
+    const response = await fetch(
+      "http://localhost:8080?" + new URLSearchParams({ q })
+    );
+
+    const data: Animal[] = await response.json();
+    setAnimals(data);
+  };
+
+  return (
+    <main>
+      <h1>Animal Farm</h1>
+
+      <input
+        type="text"
+        placeholder="Search"
+        onChange={(e) => searchHandler(e.target.value)}
+      />
+
+      <ul>
+        {animals.map((animal) => (
+          <Animal
+            key={animal.id}
+            type={animal.type}
+            name={animal.name}
+            age={animal.age}
+          ></Animal>
+        ))}
+
+        {animals.length === 0 && "No animals found"}
+      </ul>
+    </main>
+  );
+}
+
+export default App;
---
+++ animalfarm/src/components/Animal.tsx
@@ -0,0 +1,15 @@
+interface AnimalProps {
+  type?: string;
+  name?: string;
+  age?: number;
+}
+
+function Animal({ type, name, age }: AnimalProps) {
+  return (
+    <li>
+      <strong>{type}</strong> {name} ({age} years old)
+    </li>
+  );
+}
+
+export default Animal;
