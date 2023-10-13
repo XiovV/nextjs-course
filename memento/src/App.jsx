462598b10e452888d5dbb3304896a696abc0eb08
XiovV <xiovv@tutanota.com>
2023-10-13 20:24:19 +0200

    feat: add grid of buttons

--- memento/src/App.jsx
+++ memento/src/App.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import shuffle from "./utilities/shuffle";
+import Card from "./components/Card";
 
 function App() {
-  return <div className="App"></div>;
+  const [cards, setCards] = useState(shuffle);
+
+  return (
+    <>
+      <div className="grid">
+        {cards.map((card) => {
+          const { image, id, matched } = card;
+
+          return (
+            <Card key={id} image={image} selected={false} onClick={() => {}} />
+          );
+        })}
+      </div>
+    </>
+  );
 }
 
 export default App;
