49274ffda106da63364aabfe7e0ab84951fd90c9
XiovV <xiovv@tutanota.com>
2023-10-27 10:36:04 +0200

    chore: myspace project init

--- memento/src/App.jsx
+++ memento/src/App.jsx
@@ -1,9 +1,66 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import shuffle from "./utilities/shuffle";
 import Card from "./components/Card";
 
 function App() {
   const [cards, setCards] = useState(shuffle);
+  const [pickOne, setPickOne] = useState(null);
+  const [pickTwo, setPickTwo] = useState(null);
+  const [disabled, setDisabled] = useState(false);
+  const [wins, setWins] = useState(0);
+
+  const handleClick = (card) => {
+    if (!disabled) {
+      pickOne ? setPickTwo(card) : setPickOne(card);
+    }
+  };
+
+  const handleTurn = () => {
+    setPickOne(null);
+    setPickTwo(null);
+    setDisabled(false);
+  };
+
+  useEffect(() => {
+    let pickTimer;
+
+    if (!pickOne || !pickTwo) {
+      return;
+    }
+
+    if (pickOne.image !== pickTwo.image) {
+      setDisabled(true);
+
+      pickTimer = setTimeout(() => {
+        handleTurn();
+      }, 1000);
+
+      return;
+    }
+
+    setCards((prevCards) => {
+      return prevCards.map((card) => {
+        if (card.image !== pickOne.image) {
+          return card;
+        }
+
+        return { ...card, matched: true };
+      });
+    });
+
+    return () => {
+      clearTimeout(pickTimer);
+    };
+  }, [cards, pickOne, pickTwo]);
+
+  useEffect(() => {
+    const checkWin = cards.filter((card) => !card.matched);
+    if (cards.length && checkWin.length < 1) {
+      console.log("You win!");
+      setWins(wins + 1);
+      handleTurn();
+    }
+  }, [cards, wins]);
 
   return (
     <>
@@ -12,7 +69,12 @@ function App() {
           const { image, id, matched } = card;
 
           return (
-            <Card key={id} image={image} selected={false} onClick={() => {}} />
+            <Card
+              key={id}
+              image={image}
+              selected={card === pickOne || card === pickTwo || matched}
+              onClick={() => handleClick(card)}
+            />
           );
         })}
       </div>
