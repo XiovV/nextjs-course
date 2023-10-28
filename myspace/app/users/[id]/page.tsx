09e617b4e31be5a21c23fb36ec59a6a416ae3f36
XiovV <xiovv@tutanota.com>
2023-10-28 20:13:43 +0200

    feat: add user page

---
+++ myspace/app/users/[id]/page.tsx
@@ -0,0 +1,34 @@
+import { prisma } from "@/app/lib/prisma";
+import { Metadata } from "next";
+
+interface Props {
+  params: {
+    id: string;
+  };
+}
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const user = await prisma.user.findUnique({ where: { id: params.id } });
+  return { title: `User profile of ${user?.name}` };
+}
+
+export default async function UserProfile({ params }: Props) {
+  const user = await prisma.user.findUnique({ where: { id: params.id } });
+
+  const { name, bio, image } = user ?? {};
+
+  return (
+    <div>
+      <h1>{name}</h1>
+
+      <img
+        src={image ?? "/mememan.webp"}
+        alt={`${name}'s profile`}
+        width={300}
+      />
+
+      <h3>Bio</h3>
+      <p>{bio}</p>
+    </div>
+  );
+}
